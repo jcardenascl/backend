968f4b12636da9ccb00d111984a165a0a0ca5126
Daniel Esteves <estevesd8@gmail.com>
2020-02-29 23:31:28 -0400

    Working CRUD of transactions

--- src/graphql/resolvers/transaction.js
+++ src/graphql/resolvers/transaction.js
@@ -13,6 +13,62 @@ export default {
       }
 
       return transactions
+    },
+    transaction: async (_, { id }, { models: { Transaction } }) => {
+      let transaction
+
+      try {
+        transaction = await Transaction.findById({ _id: id }).exec()
+      } catch (error) {
+        errorHandler(error)
+      }
+
+      return transaction
+    }
+  },
+  Mutation: {
+    transaction: async (_, { input }, { models: { Transaction } }) => {
+      let transaction
+
+      try {
+        transaction = await Transaction.create(input)
+      } catch (error) {
+        errorHandler(error)
+      }
+
+      return transaction
+    },
+    updateTransaction: async (
+      _,
+      { id, input },
+      { models: { Transaction } }
+    ) => {
+      let transaction
+
+      try {
+        transaction = await Transaction.findOneAndUpdate(
+          { _id: id },
+          { $set: input },
+          { new: true, useFindAndModify: false }
+        )
+      } catch (error) {
+        errorHandler(error)
+      }
+
+      return transaction
+    },
+    deleteTransaction: async (_, { id }, { models: { Transaction } }) => {
+      let transaction
+
+      try {
+        transaction = await Transaction.findOneAndDelete({ _id: id })
+      } catch (error) {
+        errorHandler(error)
+      }
+
+      return transaction !== null
+        ? `Transaction:${id} deleted successfully`
+        : `Transaction:${id} didn't exist`
     }
   }
 }
--- src/models/Transaction.js
+++ src/models/Transaction.js
@@ -1,29 +1,24 @@
 // Dependencies
 import { Schema, model } from 'mongoose'
 
-const TransactionSchema = new Schema({
-  description: {
-    type: String,
-    required: [true, 'Please add a description']
+const TransactionSchema = new Schema(
+  {
+    description: {
+      type: String,
+      required: [true, 'Please add a description']
+    },
+    ammount: {
+      type: Number,
+      required: [true, 'Please add an ammount']
+    },
+    currency: {
+      type: String,
+      enum: ['VEF', 'USD'],
+      required: [true, 'Please choose a correct currency: VEF or USD']
+    }
   },
-  ammount: {
-    type: Number,
-    required: [true, 'Please add an ammount']
-  },
-  currency: {
-    type: String,
-    enum: ['VEF', 'USD'],
-    required: [true, 'Please choose a correct currency: VEF or USD']
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now
-  },
-  updatedAt: {
-    type: Date,
-    default: Date.now
-  }
-})
+  { timestamps: true }
+)
 
 const Transaction = model('transaction', TransactionSchema)
 
